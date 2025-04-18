d7ec45a9c15aef195ce0c6b182c94f7b4b47eed8
YourTooSlow (YTS) <mekhdoura@gmail.com>
2025-04-18 19:22:06 -0400

    added secret search, and js
    this is a pretty big update. i replaced all the htmls to have secret search in their nav bar now. secrets.html has a list of inputs for secret search

---
+++ js/secretsearch.js
@@ -0,0 +1,44 @@
+function checkSecret() {
+    const input = document.getElementById("secretInput");
+    const keyword = input.value.toLowerCase().trim();
+  
+    const secretPages = {
+      test: "secret-htmls/test.html",
+      dsi: "secret-htmls/dsi.html"
+    };
+  
+    const accessMessage = document.getElementById("accessMessage");
+    const deniedMessage = document.getElementById("deniedMessage");
+  
+    accessMessage.style.display = "none";
+    deniedMessage.style.display = "none";
+    accessMessage.style.opacity = "0";
+    deniedMessage.style.opacity = "0";
+  
+    if (secretPages[keyword]) {
+      const unlockSound = new Audio("bgm/unlock.mp3");
+      unlockSound.volume = 1.0;
+      unlockSound.muted = false;
+      unlockSound.play().catch((err) => {
+        console.error("Failed to play unlock sound:", err);
+      });
+  
+      accessMessage.style.display = "block";
+      setTimeout(() => {
+        accessMessage.style.opacity = "1";
+      }, 50);
+  
+      setTimeout(() => {
+        window.location.href = secretPages[keyword];
+      }, 1500);
+    } else {
+      
+      deniedMessage.style.display = "block";
+      setTimeout(() => {
+        deniedMessage.style.opacity = "1";
+      }, 50);
+  
+    }
+  }
+  
+  
