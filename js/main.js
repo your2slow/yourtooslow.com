1676ebb36ed1abdbec04080081a638709a15877a
YourTooSlow (YTS) <mekhdoura@gmail.com>
2025-04-22 22:34:06 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,36 +1,38 @@
 function showLoadingAndRedirect(url) {
-    const icon = document.getElementById('loading-icon');
-    const sound = document.getElementById('loading-sfx');
-  
-    icon.style.display = 'block';
-    sound.currentTime = 0;
-    sound.play();
-  
-    // white screen fade :tro:
-    document.body.classList.add('loading-fade');
-  
-    setTimeout(() => {
-      window.location.href = url;
-    }, 1500); // 1.5 second delay!
-  }
+  const icon = document.getElementById('loading-icon');
+  const sound = document.getElementById('loading-sfx');
 
-  document.addEventListener('click', () => {
-    const clickSound = document.getElementById('click-sfx');
-    if (clickSound) {
-      clickSound.currentTime = 0; 
-      clickSound.play().catch(e => console.error('Click sound error:', e));
-    } else {
-      console.log('click-sfx not found');
-    }
+  icon.style.display = 'block';
+  sound.currentTime = 0;
+  sound.play();
+
+  document.body.classList.add('loading-fade');
+
+  setTimeout(() => {
+    window.location.href = url;
+  }, 1500);
+  setTimeout(stoploading,4300); 
+}
+
+document.querySelectorAll('a[href]').forEach(link => {
+  const href = link.getAttribute('href');
+  if (href && !href.startsWith('#') && !href.startsWith('javascript')) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const clickSfx = document.getElementById('click-sfx');
+      if (clickSfx) {
+        clickSfx.currentTime = 0;
+        clickSfx.play();
+      }
+      showLoadingAndRedirect(href);
+
+    });
+  }
+}); 
+
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text).then(() => {
+    alert('Copied Discord username to clipboard!');
   });
-  
-  document.querySelectorAll('a[href]').forEach(link => {
-    const href = link.getAttribute('href');
-    if (href && !href.startsWith('#') && !href.startsWith('javascript')) {
-      link.addEventListener('click', function (e) {
-        e.preventDefault();
-        showLoadingAndRedirect(href);
-      });
-    }
-  });
-  
+}
